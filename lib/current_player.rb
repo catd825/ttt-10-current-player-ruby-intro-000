8e293c8fddc876244e0dd30892d457c895c2611d
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 15:56:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,11 @@
 def turn_count(board)
-
-  counter = 0
-  board.each do |turn|
-
-    puts "#{turn}"
-    counter +=1
+    counter = 0
+    board.each do |turn|
+      puts "#{turn}"
+      if turn == "X" || turn == "O"
+          counter +=1
+      else
+    turn_count(board)
+    end
   end
-
 end
